d98d7ca839b70f30f330ab13269f887b2da91486
kaveh <kaveh@sourceforge.net>
2005-12-20 14:13:21 +0000

    Adding body to CollectionManager

--- lib/CollectionManager.py
+++ lib/CollectionManager.py
@@ -22,6 +22,9 @@ sys.path.insert(1,sys.path[0] + '/../lib/importedLibs')
 
 import PXPaths
 from Logger import Logger
+import CollectionBuilder
+import CollectionConfigParser
+import time
 
 PXPaths.normalPaths()              # Access to PX paths
 
@@ -34,9 +37,22 @@ class CollectionManager(object):
            Author:      National Software Development<nssib@ec.gc.ca>
            Date:        December 2005
     """
+
     def __init__(self, source, logger=None):
         self.source = source   # Source object containing configuration infos about the collector
         self.logger = logger   # Logger object
+
+        #-----------------------------------------------------------------------------------------
+        # Creating CollectionBuilder object to give us access to CollectionBuilder methods
+        #-----------------------------------------------------------------------------------------
+        self.collectionBuilder = CollectionBuilder.CollectionBuilder(self.logger) 
+
+        #-----------------------------------------------------------------------------------------
+        # Creating CollectionConfig object to give us access and control over collection 
+        # config parameters
+        #-----------------------------------------------------------------------------------------
+        self.collectionConfig = CollectionConfigParser.CollectionConfigParser(self.logger,self.source)
+
 
     def collectReport(self, fileName):
         """ collectReports (self, fileName)
@@ -52,12 +68,44 @@ class CollectionManager(object):
                 EmptyString: Empty Text sring - Indicating that the bulleting does not
                                                 need to be sent immediately.
         """
-        self.logger.info("collectReports() has been called")
-        file = open(fileName, 'r')
-        rawBulletin = file.read()
-        self.logger.info("** In collector, the bulletin reads: %s" % rawBulletin)
-        self.logger.info("Source.type in collector is: %s" % self.source.type)
-        return rawBulletin
+      
+        #-----------------------------------------------------------------------------------------
+        # use collectionBuilder to create a bulletin object from the given file
+        #-----------------------------------------------------------------------------------------
+        bulletin = self.collectionBuilder.buildBulletinFromFile(fileName)
+
+        #-----------------------------------------------------------------------------------------
+        # Let's find out if the report arrived on time
+        #-----------------------------------------------------------------------------------------
+        if (self.isReportOnTime(bulletin)):
+            self.logger.info("COMPLETEME: The report was on time")
+        else:
+            self.logger.info("COMPLETEME: The report was NOT on time")
+
+        #-----------------------------------------------------------------------------------------
+        # REMOVEME
+        #-----------------------------------------------------------------------------------------
+        #self.logger.info("Source.type in collector is: %s" % self.source.type)
+        #self.logger.info("The new bulletin's Header is: %s" % bulletin.getHeader())
+        #self.logger.info("The new bulletin's time is: %s" % bulletin.getTimeStamp())
+
+        #-----------------------------------------------------------------------------------------
+        # COMPLETEME
+        #-----------------------------------------------------------------------------------------
+        return 'THIS IS A FAKE RETURN IN CollectionManager.py'
+
+
+    def isReportOnTime(self, bulletin):
+        """ isReportOnTime(bulletin) -> Boolean
+
+            Given the bulletin, returns True if the bulletin is considered on time
+            and an empty string if the bulletin is not considered on time.
+        """
+        presentTime = time.strftime("%d%H%M",time.localtime())
+        bulletinTime = bulletin.getTimeStamp()
+
+        self.logger.info("COMPLETEME presentTime is: %s" % presentTime)
+        self.logger.info("COMPLETEME bulletin time is: %s" % bulletinTime)
 
 if __name__ == '__main__':
     pass
