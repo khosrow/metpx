eebe4fbb460b8317e182973de351a6472e51cca3
kaveh <kaveh@sourceforge.net>
2006-01-20 22:03:46 +0000

    Cleaned up methods

--- lib/CollectionScheduler.py
+++ lib/CollectionScheduler.py
@@ -13,7 +13,7 @@
 #               This class is similar to the receiverAM class.
 #
 # Revision History: 
-#   COMPLETEME -for now this is mostly a paste of receiverAM
+#   
 #               
 #############################################################################################
 """
@@ -126,9 +126,9 @@ class CollectionScheduler(threading.Thread,gateway.gateway):
         self.sendLateCollections()
 
         #-----------------------------------------------------------------------------------------
-        # Cleanup old files
+        # Cleanup old directories and files under the /apps/px/collection sub-tree
         #-----------------------------------------------------------------------------------------
-        #self.purgeOldFiles()
+        self.purgeOldDirsAndFiles()
 
         #-----------------------------------------------------------------------------------------
         # sleep until next event
@@ -379,8 +379,22 @@ class CollectionScheduler(threading.Thread,gateway.gateway):
            return False
        
 
+    def purgeOldDirsAndFiles(self):
+        """ purgeOldDirsAndFiles()
 
+            This method removes items of this type (SA, SI, SM) which have
+            exceeded the time-to-live value
 
+        """
+        #-----------------------------------------------------------------------------------------
+        # Clean myRootDir (/apps/px/collection/<idType>)
+        #-----------------------------------------------------------------------------------------
+        # COMPLETEME
+
+        #-----------------------------------------------------------------------------------------
+        # Clean control dir (/apps/px/collection/control/<idType>)
+        #-----------------------------------------------------------------------------------------
+        # COMPLETEME
 
 
 
@@ -420,27 +434,6 @@ class CollectionScheduler(threading.Thread,gateway.gateway):
 
 
 
-
-    def write(self,data):
-        """### Ajout de receiverAm ###
-
-           L'écrivain est un bulletinManagerAm.
-
-           Visibilité:  Privée
-           Auteur:      Louis-Philippe Thériault
-           Date:        Octobre 2004
-        """
-
-        self.logger.veryveryverbose("%d nouveaux bulletins seront écrits" % len(data))
-
-        while True:
-            if len(data) <= 0:
-                break
-
-            rawBulletin = data.pop(0)
-
-            self.unBulletinManager.writeBulletinToDisk(rawBulletin,includeError=True)
-
     def reloadConfig(self):
         self.logger.info('Demande de rechargement de configuration')
 
@@ -471,150 +464,7 @@ class CollectionScheduler(threading.Thread,gateway.gateway):
 
             self.logger.debug("Erreur: %s", str(e.args))
         
-    def getTwoLetterReportType():
-        """
-            /* No Comment */
-        """
-        return self.twoLetterReportType
     
-    def _findAScheduledCollectionToSend():
-        """
-            _findAScheduledCollectionToSend():
-            Search this CollectionScheduler's root dir for a CollectionBulletin that's ready to be sent.  
-            Build the collection. 
-            Return it.
-
-            _findAScheduledCollectionToSend() is a brute force search for collections that are
-            ready to be sent.   Since the first match is returned, this lgorithm should
-            be relatively fast.
-        """
-        msgTypeDirs = self._listDirs(self.rootDir)
-        for dateDir in dateDirs :
-            originDirs = self._listDirs(self.rootDir + "/" + dateDir)
-            for originDir in originDirs :
-                fullMsgTypeDirs = self._listDirs(self.rootDir + "/" + dateDir + "/" + originDir)
-                for fullMsgTypeDir in fullMsgTypeDirs :
-                    bbbDirs = self._listDirs(self.rootDir + "/" + dateDir + "/" + originDir + "/" + fullMsgTypeDir)
-                    for bbbDir in bbbDirs:
-                        pathToCollection = self.rootDir + "/" + dateDir + "/" + originDir + "/" + fullMsgTypeDir + "/" + bbbDir
-                        if self.collectionShouldBeSent(dateDir, bbbDir):
-                            tempbullColl = self.buildBulletinCollection(dateDir, originDir, fullMsgTypeDir, bbbDir)
-                            # FIXME: check that tempbullColl is not null, maybe & some other error checking
-                            return bullColl
-        return 
-
-    def _collectionShouldBeSent(self, dateDir, bbbDir):
-        """
-            Check to see if a bbbDir is _sent or _busy.  Also, ontime collections should not be sent until
-            after the headerValidTime.
-            If the collection is already sent, currently busy, or an ontime collection that's still 
-            before it's validTime, then return false. 
-            Otherwise, return True
-        """
-        #Build the set of flags that mark directories that are not to be sent.
-        ignoreSet = [ self.collectionConfig.getBusyCollectionToken(), self.collectionConfig.getSentCollectionToken() ]
-        if containsAny(bbbDir, ignoreSet):
-            self.logger.error('REMOVEME: found a sent/busy flag using in: %s', bbbDir)
-            return False
-        if contaisAny(bbbDir, ONTIME_FLAG_CONSTANT):
-            # compare the ontime flag to the present time to see if it's time to send it.
-            if not self._isOnTimeBulletinReadyYet(self.twoLetterReportType, dateDir):
-                return False
-        return True
-        
-    def _buildBulletinCollection(self, date, origin, fullMsgType, bbb):
-        """
-            _buildBulletinCollection(self, date, origin, fullMsgType, bbb) returns a BulletinCollection 
-            object after reading it from disk from a path generated from the given parameters.
-
-            The given parameters match the directory structure of the collection DB.
-        """
-        # REMOVEME unless this code is used to change th einput parameters
-        #fullPathList = split(fullPath, "/", 5)
-        #date = fullPathList[1]
-        #origin = fullPathList[2]
-        #fullMsgType = fullPathList[3]
-        #bbb = fullPathList[4]
-
-        ############################################################################################ 
-        # build the path to the collection
-        ############################################################################################
-        pathToCollection = self.rootDir + "/" + date + "/" + origin + "/" + fullMsgType + "/" + bbb
-
-        bullColl = BulletinCollection.BulletinCollection()
-        if not containsAny(pathToDir, ONTIME_FLAG_CONSTANT):
-            bullColl.setBBB(bbb)
-        
-        ############################################################################################ 
-        # set the attribute variables of the bulletinCollection
-        ############################################################################################
-        # set the header
-        bullColl.setHeader(fullMsgType + " " + origin + " " + date) 
-        # set the body.  Each line of the BulletinCollection body will be the body of each bulletin
-
-        numBulletins = 0
-        bullList=os.listdir(pathToCollection)
-        for bullFileName in bullList :
-            fd = open(bullFileName, O_RDONLY)
-            header = fd.readline() 
-
-            #########################################################################################
-            # Sanity check: test that the bulletin header matches the collection header
-            #########################################################################################
-            headerList = split(header, " ", 3)
-            if headerList[0] != fullMsgType or headerList[1] != origin or headerList[2] != date:
-                self.logger.warning("WARNING: bulletin file found that does not match the expected header.  %s" %bullFileName)
-                continue
-
-            #########################################################################################
-            # get the body of the bulletin and add it to the collection
-            #########################################################################################
-            bulletinBody = fd.read() # read the entire bulletin body
-            bullColl.bulletin[numBulletins] = bulletinBody
-            numBulletins = numBulletins + 1
-
-            fd.close()
-
-        return bullColl
-
-    def _findAScheduledCollectionToSend(self):
-        """ 
-            This method searches the collection database for a collection that needs to be sent at this time
-            FIXME: This method should wrap access to the diskWritter!!
-
-        """
-        self.logger.error('REMOVEME: finding a collection to send')
-        self.logger.error('REMOVEME: collectionDB is: %s', PXPaths.COLLECTION_DB)
-
-        #################################################################
-        # Linearly search through every directory that represents a collection.
-        #################################################################
-        msgTypeDirs = self._listDirs(PXPaths.COLLECTION_DB)
-        for msgTypeDir in msgTypeDirs:
-            timeDirs = self._listDirs(PXPaths.COLLECTION_DB + msgTypeDir)
-            for timeDir in timeDirs:
-                originDirs = self._listDirs(PXPaths.COLLECTION_DB + msgTypeDir + "/" + timeDir)
-                for originDir in originDirs:
-                    bbbDirs = self._listDirs(PXPaths.COLLECTION_DB + msgTypeDir + "/" + timeDir + "/" + originDir)
-                    for bbbDir in bbbDirs:
-                        pathToPossibleCollection = PXPaths.COLLECTION_DB + msgTypeDir + "/" + timeDir + "/" + originDir + "/" + bbbDir
-                        #################################################################
-                        #  We've found a path to a directory representing a collection,
-                        #  we now need to test if this collection should be sent now.
-                        #################################################################
-                        self.logger.error('REMOVEME: found possibleCollection: %s', pathToPossibleCollection)
-                        if self._collectionShouldBeSent(msgTypeDir, timeDir, originDir, bbbDir):
-                            return self.formBulletinCollection(msgTypeDir, timeDir, originDir, bbbDir)
-        # No collections are to be sent at this time, so return NULL
-        return ""
-    
-    def _listDirs(self, rootDir):
-        """
-            return a non recursive list of subdirectories (and files?(FIXME)) in the given directory
-        """
-        dirList=os.listdir(rootDir)
-        return dirList
-
     def _calcDurationUntilNextEvent():
         """
             This will calculate the number of seconds (rounding up [hopfully]) until the next scheduled
@@ -638,39 +488,6 @@ class CollectionScheduler(threading.Thread,gateway.gateway):
         return 60*collectionConfig.getLateCycleByHeader(self.getTwoLetterReportType())
 
 
-    def _isOnTimeBulletinReadyYet(self, collectionType, collectionTime):
-        """
-           returns bool
-           returns True if the given ontime collection should have been sent by now, ie the current time 
-           exceeds the appropriate headerValidTime for the collection detais given.
-           returns False otherwise
-
-           we know a collection is ripe to be sent if:
-               - the collection hour is different than the current hour
-               - the collection hour is the same as the current hour but,
-                    the current minutes are greater or equal to the headerValidTime
-
-            This logic could have the unfortunate effect of not properly identifying ripe collections 
-            that have a different date, but same hour.
-
-        """
-        presentDateTime = datetime.datetime.now()
-
-        ############################################################################
-        # Compare hour field
-        ############################################################################
-        collectionHour = collectionTime[2] + collectionTime[3]
-        self.logger.error('REMOVEME: calculated collectionHour for %s is %s', collectionTime, collectionHour)
-        if presentDateTime.hour != collectionHour:
-            return True
-        else:
-            ############################################################################
-            # Compare minutes field since hour field are identical
-            ############################################################################
-            collectionMinute = collectionTime[4] + collectionTime[5]
-            collectionValidTime = "COMPLETEME"
-            if collectionMinute >= collectionValidTime :
-                return True
-        return False   
+    
 
 
