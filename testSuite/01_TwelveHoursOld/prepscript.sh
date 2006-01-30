a7d68268fe8f431e766a8d28274a90994d308258
kaveh <kaveh@sourceforge.net>
2006-01-30 18:56:05 +0000

    Fixing script

--- testSuite/01_TwelveHoursOld/prepscript.sh
+++ testSuite/01_TwelveHoursOld/prepscript.sh
@@ -9,14 +9,17 @@ INPUTDIR=`pwd`/input
 # for each file in the inputdir,
 for bulletin in `ls $INPUTDIR`
 do
-   #echo "INPUTDIR is $INPUTDIR"
-   #echo "bulletin is $bulletin"
-   # extract the date field from the bulletin
-   OLDDATE=`cat $INPUTDIR/$bulletin |cut -d' ' -f3|head -1`
-   #echo "OLDDATE is: $OLDDATE"
-   # calculate the date X hours ago
-   NEWDATE=`date -d "-$NUM_HOURS_OLD hour" +%d%H%M`
-   #echo "NEWDATE is: $NEWDATE"
+   if [ -f $bulletin ]
+   then
+     #echo "INPUTDIR is $INPUTDIR"
+     #echo "bulletin is $bulletin"
+     # extract the date field from the bulletin
+     OLDDATE=`cat $INPUTDIR/$bulletin |cut -d' ' -f3|head -1`
+     #echo "OLDDATE is: $OLDDATE"
+     # calculate the date X hours ago
+     NEWDATE=`date -d "-$NUM_HOURS_OLD hour" +%d%H%M`
+     #echo "NEWDATE is: $NEWDATE"
    
-   ../_searchAndChangeInFiles.sh "$INPUTDIR" $bulletin $OLDDATE $NEWDATE
+     ../_searchAndChangeInFiles.sh "$INPUTDIR" $bulletin $OLDDATE $NEWDATE
+   fi
 done
--- testSuite/03_MultipleCCandAABulletins/prepscript.sh
+++ testSuite/03_MultipleCCandAABulletins/prepscript.sh
@@ -10,14 +10,17 @@ INPUTDIR="${INPUTDIR}/input"
 # for each file in the inputdir,
 for bulletin in `ls $INPUTDIR`
 do
-   #echo "INPUTDIR is $INPUTDIR"
-   #echo "bulletin is $bulletin"
-   # extract the date field from the bulletin
-   OLDDATE=`cat $INPUTDIR/$bulletin |cut -d' ' -f3|head -1`
-   echo "OLDDATE is: $OLDDATE"
-   # calculate the date X hours ago
-   NEWDATE=`date -d "-$NUM_HOURS_OLD hour" +%d%H%M`
-   echo "NEWDATE is: $NEWDATE"
+   if [ -f $bulletin ]
+   then
+     #echo "INPUTDIR is $INPUTDIR"
+     #echo "bulletin is $bulletin"
+     # extract the date field from the bulletin
+     OLDDATE=`cat $INPUTDIR/$bulletin |cut -d' ' -f3|head -1`
+     echo "OLDDATE is: $OLDDATE"
+     # calculate the date X hours ago
+     NEWDATE=`date -d "-$NUM_HOURS_OLD hour" +%d%H%M`
+     echo "NEWDATE is: $NEWDATE"
    
-   /apps/px/test/_searchAndChangeInFiles.sh "$INPUTDIR" $bulletin $OLDDATE $NEWDATE
+     ../_searchAndChangeInFiles.sh "$INPUTDIR" $bulletin $OLDDATE $NEWDATE
+   fi  
 done
