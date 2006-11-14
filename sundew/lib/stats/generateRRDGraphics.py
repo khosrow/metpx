dd751742780c72f43cc0e268d54217d3f5ae3979
Nicholas Lemay <nicholaslemay@sourceforge.net>
2006-11-14 22:20:54 +0000

    Now supports addition of new clients/sources that dont have data for the entire span of the graphic to others that do.

--- sundew/lib/stats/generateRRDGraphics.py
+++ sundew/lib/stats/generateRRDGraphics.py
@@ -501,7 +501,7 @@ def getGraphicsMinMaxMean( databaseName, startTime, endTime, interval, logger = 
 
         for meanTuple in meanTuples :            
             if meanTuple[0] != 'None' and meanTuple[0] != None :
-                realValue = ( meanTuple[0] * interval ) 
+                realValue = ( float(meanTuple[0]) * float(interval) ) 
                 if  realValue > max:
                     max = realValue
                 if realValue < min or min == None :
@@ -709,7 +709,11 @@ def createNewDatabase( fileType, type, machine, start, infos, logger ):
         Creates a brand new database bases on fileType,type and machine.
         
         If a database with the same name allready exists, it will be removed.
-         
+        
+        Databases are created with rras that are identical to those found in the 
+        transferPickleToRRD.py file. If databases are to change there they must be
+        changed here also.  
+    
     """
     
     combinedDatabaseName = PXPaths.STATS + "databases/combined/%s_%s_%s" %( fileType, type, machine )
@@ -724,19 +728,19 @@ def createNewDatabase( fileType, type, machine, start, infos, logger ):
     if infos.timespan <(5*24):# daily :
         start = start - 60
         rrdtool.create( combinedDatabaseName, '--start','%s' %( start ), '--step', '60', 'DS:%s:GAUGE:60:U:U' %type,'RRA:AVERAGE:0:1:7200','RRA:MIN:0:1:7200', 'RRA:MAX:0:1:7200' )
-        print "daily"     
-    elif infos.timespan >(5*24) and infos.timespan <=(14*24):# weekly :  
+        #print "daily"     
+    elif infos.timespan >(5*24) and infos.timespan <(14*24):# weekly :  
         start = start - (60*60)
         rrdtool.create( combinedDatabaseName, '--start','%s' %( start ), '--step', '3600', 'DS:%s:GAUGE:3600:U:U' %type,'RRA:AVERAGE:0:1:336','RRA:MIN:0:1:336', 'RRA:MAX:0:1:336' )               
-        print "weekly"            
+        #print "weekly"            
     elif infos.timespan <(365*24):#monthly
         start = start - (240*60)
         rrdtool.create( combinedDatabaseName, '--start','%s' %( start ), '--step', '14400', 'DS:%s:GAUGE:14400:U:U' %type, 'RRA:AVERAGE:0:1:1460','RRA:MIN:0:1:1460','RRA:MAX:0:1:1460' )  
-        print "monthly"
+        #print "monthly"
     else:#yearly
         start = start - (1440*60)
         rrdtool.create( combinedDatabaseName, '--start','%s' %( start ), '--step', '86400', 'DS:%s:GAUGE:86400:U:U' %type, 'RRA:AVERAGE:0:1:3650','RRA:MIN:0:1:3650','RRA:MAX:0:1:3650' ) 
-        print "yearly"
+        #print "yearly"
     
     return combinedDatabaseName
     
@@ -766,8 +770,20 @@ def getPairsFromAllDatabases( type, machine, start, end, infos, logger=None ):
     for i in range( len(typeData[client]) ) :#make total of all clients for every timestamp
         total = 0 
         for client in infos.clientNames:
-            total = total + float( typeData[client][i].split( ":" )[1].replace(" ", "") )
-            #print " %s %s : %s " %(client,type,typeData[client][i].split( ":" )[1].replace(" ", ""))
+            try : 
+                data = typeData[client][i].split( ":" )[1].replace(" ", "")
+                if data != None and data != 'None':
+                    total = total + float( data )
+                elif logger != None: 
+                    logger.warning( "Could not find data for %s for present timestamp." %(client) )
+                         
+            except:
+                if logger != None :
+                    #print "Could not find data for %s for present timestamp." %(client)
+                    logger.warning( "Could not find data for %s for present timestamp." %(client) )
+                
+                
+        #print " %s %s : %s " %(client,type,typeData[client][i].split( ":" )[1].replace(" ", ""))
         if type == "latency":#latency is always an average
             total = total / ( len( output ) )
                         
@@ -790,8 +806,8 @@ def createMergedDatabases( infos, logger = None ):
          
     """            
 
-    dataPairs = {}
-    typeData = {}
+    dataPairs  = {}
+    typeData   = {}
     end        = int ( MyDateLib.getSecondsSinceEpoch ( infos.endDate ) )  
     start      = end - ( infos.timespan * 60 * 60 ) 
     databaseNames = {}
@@ -808,9 +824,9 @@ def createMergedDatabases( infos, logger = None ):
             databaseNames[type] = combinedDatabaseName                            
             
             #update database with pairs 
-            print "***********%s***********" %type       
+            #print "***********%s***********" %type       
             for pair in pairs:
-                print '%s:%s' %( int(pair[0]), pair[1] ) 
+            #    print '%s:%s' %( int(pair[0]), pair[1] ) 
                 rrdtool.update( combinedDatabaseName, '%s:%s' %( int(pair[0]), pair[1] ) )
                  
                 
@@ -824,11 +840,11 @@ def generateRRDGraphics( infos, logger = None ):
         
     """    
     
-    print "************************************%s" %infos.endDate
-    if infos.totals  :
+    #print "************************************%s" %infos.endDate
+    if infos.totals: #Graphics based on total values from a group of clients.
         databaseNames = createMergedDatabases( infos, logger )
         
-        #Make 2 title option either tx or list of clients
+        #Make 2 title filename options either tx/rx...or list of clients
         for machine in infos.machines:
             for type in infos.types:
                 plotRRDGraph( databaseNames[type], type, infos.fileType, infos.fileType, machine, infos, lastUpdate =  MyDateLib.getSecondsSinceEpoch(infos.endDate), logger =logger )
