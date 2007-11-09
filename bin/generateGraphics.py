dca359d7138d037360362eb7a8045420288799a8
Nicholas Lemay <nicholaslemay@sourceforge.net>
2007-11-09 18:51:31 +0000

    Commiting first version of this file which is built to support multiple languages.

--- bin/generateGraphics.py
+++ bin/generateGraphics.py
@@ -21,7 +21,7 @@ named COPYING in the root of the source directory tree.
 ##############################################################################
 """
 
-import os, time, sys
+import gettext, os, time, sys
 sys.path.insert(1, sys.path[0] + '/../../')
 
 from optparse import OptionParser
@@ -30,13 +30,13 @@ from fnmatch import fnmatch
 from pxStats.lib.StatsDateLib import StatsDateLib
 from pxStats.lib.StatsPaths import StatsPaths
 from pxStats.lib.GeneralStatsLibraryMethods import GeneralStatsLibraryMethods
-from pxStats.lib.ClientGraphicProducer import ClientGraphicProducer
+from pxStats.lib.ClientGraphicProducerTest import ClientGraphicProducer
 
 LOCAL_MACHINE = os.uname()[1]
 
 class _GraphicsInfos:
     
-    def __init__( self, directory, fileType, types, collectUpToNow,  clientNames = None ,  timespan = 12, currentTime = None, productTypes = ["All"], machines = ["pdsGG"], copy = False, combineClients = False, groupName = ""  ):
+    def __init__( self, directory, fileType, types, collectUpToNow,  clientNames = None ,  timespan = 12, currentTime = None, productTypes = None, machines = None, copy = False, combineClients = False, groupName = ""  ):
 
             
         self.directory      = directory         # Directory where log files are located. 
@@ -46,8 +46,8 @@ class _GraphicsInfos:
         self.clientNames    = clientNames or [] # Client name we need to get the data from.
         self.timespan       = timespan          # Number of hours we want to gather the data from. 
         self.currentTime    = currentTime       # Time when stats were queried.        
-        self.productTypes   = productTypes      # Specific data type on wich we'll collect the data.
-        self.machines       = machines          # Machine from wich we want the data to be calculated.
+        self.productTypes   = productTypes or [ _("All")]     # Specific data type on wich we'll collect the data.
+        self.machines       = machines or []    # Machine from wich we want the data to be calculated.
         self.copy           = copy              # Whether or not we create a copy file.
         self.combineClients = combineClients    # Wheter or not we combine the data of all clients/sources.
         self.groupName      = groupName         # Name given to the combined client/sources.
@@ -96,9 +96,9 @@ def getOptionsFromParser( parser ):
         currentTime = "%s %s" %( split[0], split[1] )
 
     except:    
-        print "Error. The date format must be YYYY-MM-DD HH:MM:SS" 
-        print "Use -h for help."
-        print "Program terminated."
+        print _("Error. The date format must be YYYY-MM-DD HH:MM:SS" )
+        print _("Use -h for help.")
+        print _("Program terminated.")
         sys.exit()
     
     
@@ -108,43 +108,43 @@ def getOptionsFromParser( parser ):
                 
     except:
         
-        print "Error. The timespan value needs to be an integer one above 0." 
-        print "Use -h for help."
-        print "Program terminated."
+        print _("Error. The timespan value needs to be an integer one above 0." )
+        print _("Use -h for help.")
+        print _("Program terminated.")
         sys.exit()
         
     
     if fileType != "tx" and fileType != "rx":
-        print "Error. File type must be either tx or rx."
-        print 'Multiple types are not accepted.' 
-        print "Use -h for additional help."
-        print "Program terminated."
+        print _("Error. File type must be either tx or rx.")
+        print _('Multiple types are not accepted.' )
+        print _("Use -h for additional help.")
+        print _("Program terminated.")
         sys.exit()    
     
     
     if groupName != "" and combineClients == False :
-        print "Error. -g|--groupeName option requires the --combineClients options."
-        print 'Group names are otherwise irrelevant.' 
-        print "Use -h for additional help."
-        print "Program terminated."
+        print _("Error. -g|--groupeName option requires the --combineClients options.")
+        print _('Group names are otherwise irrelevant.' )
+        print _("Use -h for additional help.")
+        print _("Program terminated.")
         sys.exit()          
         
         
     try :
     
         if fileType == "tx":       
-            validTypes = [ "errors","filecount","bytecount","latency" ]
+            validTypes = [ _("errors"), _("filecount"), _("bytecount"), _("latency") ]
             
-            if types[0] == "All":
+            if types[0] == _("All"):
                 types = validTypes
             else :
                 for t in types :
                     if t not in validTypes:
                         raise Exception("")
         else:
-            validTypes = [ "errors","filecount","bytecount" ]
+            validTypes = [ _("errors"), _("filecount"), _("bytecount") ]
             
-            if types[0] == "All":
+            if types[0] == _("All"):
                 types = validTypes
             
             else :
@@ -154,10 +154,10 @@ def getOptionsFromParser( parser ):
 
     except:    
         
-        print "Error. With %s fileType, possible data types values are : %s." %( fileType,validTypes )
-        print 'For multiple types use this syntax : -t "type1,type2"' 
-        print "Use -h for additional help."
-        print "Program terminated."
+        print _("Error. With %s fileType, possible data types values are : %s.") %( fileType,validTypes )
+        print _('For multiple types use this syntax : -t "type1,type2"') 
+        print _("Use -h for additional help.")
+        print _("Program terminated.")
         sys.exit()
     
     
@@ -182,7 +182,7 @@ def createAParser( ):
     
     """
     
-    usage = """
+    usage = _("""
 
 %prog [options]
 ********************************************
@@ -226,7 +226,7 @@ Ex3: %prog -c satnet -d '2006-06-30 05:15:00'--> Client satnet, Date of call 200
 Ex4: %prog -c satnet -t "errors,latency"     --> Uses current time, client satnet and collect those 2 types.
 ********************************************
 * See /doc.txt for more details.           *
-********************************************"""   
+********************************************""" )  
     
     parser = OptionParser( usage )
     addOptions( parser )
@@ -241,31 +241,63 @@ def addOptions( parser ):
         
     """      
     
-    parser.add_option("-c", "--clients", action="store", type="string", dest="clients", default="satnet",
-                        help="Clients' names")
+    parser.add_option("-c", "--clients", action="store", type="string", dest="clients", default="",
+                        help=_("Clients' names") )
     
-    parser.add_option( "--copy", action="store_true", dest = "copy", default=False, help="Create a copy file for the generated image.")
+    parser.add_option( "--copy", action="store_true", dest = "copy", default=False, help=_("Create a copy file for the generated image.") )
    
-    parser.add_option( "--combineClients", action="store_true", dest = "combineClients", default=False, help="Combine the data of all client into a single graphics for each graphic type.")
+    parser.add_option( "--combineClients", action="store_true", dest = "combineClients", default=False, help=_("Combine the data of all client into a single graphics for each graphic type.") )
     
-    parser.add_option("-d", "--date", action="store", type="string", dest="currentTime", default=StatsDateLib.getIsoFromEpoch( time.time() ), help="Decide current time. Usefull for testing.")
+    parser.add_option("-d", "--date", action="store", type="string", dest="currentTime", default=StatsDateLib.getIsoFromEpoch( time.time() ), help=_("Decide current time. Usefull for testing.") )
     
-    parser.add_option("-f", "--fileType", action="store", type="string", dest="fileType", default='tx', help="Type of log files wanted.")                     
+    parser.add_option("-f", "--fileType", action="store", type="string", dest="fileType", default='tx', help=_("Type of log files wanted.") )                     
     
     parser.add_option( "-g", "--groupName", action="store", type="string", dest="groupName", default="",
-                        help="Specify a name for the combined graphics of numerous client/sources. Note : requires the --combinedClients options to work." )       
+                        help=_("Specify a name for the combined graphics of numerous client/sources. Note : requires the --combinedClients options to work." ) )       
     
-    parser.add_option( "-m", "--machines", action="store", type="string", dest="machines", default=LOCAL_MACHINE, help = "Machines for wich you want to collect data." ) 
+    parser.add_option( "-m", "--machines", action="store", type="string", dest="machines", default=LOCAL_MACHINE, help = _("Machines for wich you want to collect data.") ) 
     
-    parser.add_option("-n", "--collectUpToNow", action="store_true", dest = "collectUpToNow", default=False, help="Collect data up to current second.")
+    parser.add_option("-n", "--collectUpToNow", action="store_true", dest = "collectUpToNow", default=False, help=_("Collect data up to current second.") )
     
-    parser.add_option("-p", "--products", action="store", type = "string", dest = "productTypes", default="All", help="Specific product types to look for in the data collected.")
+    parser.add_option("-p", "--products", action="store", type = "string", dest = "productTypes", default=_("All"), help=_("Specific product types to look for in the data collected.") )
     
-    parser.add_option("-s", "--span", action="store",type ="int", dest = "timespan", default=12, help="timespan( in hours) of the graphic.")
+    parser.add_option("-s", "--span", action="store",type ="int", dest = "timespan", default=12, help=_("timespan( in hours) of the graphic.") )
        
-    parser.add_option("-t", "--types", type="string", dest="types", default="All",help="Types of data to look for.")   
+    parser.add_option("-t", "--types", type="string", dest="types", default=_("All"),help=_("Types of data to look for.") )   
+
          
+
+def  setGlobalLanguageParameters( language = 'fr'):
+    """
+        @summary : Sets up all the needed global language 
+                   variables so that they can be used 
+                   everywhere in this program.
+        
+        
+        @param language: Language that is to be 
+                         outputted by this program. 
+     
+        @return: None
+        
+    """
     
+    global LANGUAGE 
+    global translator
+    global _ 
+
+    
+    LANGUAGE = language 
+    
+    if language == 'fr':
+        fileName = StatsPaths.STATSLANGFRBIN + "generateGraphics" 
+    elif language == 'en':
+        fileName = StatsPaths.STATSLANGENBIN + "generateGraphics"     
+    
+    translator = gettext.GNUTranslations(open(fileName))
+    
+    _ = translator.gettext    
+    
+
 
 def main():
     """
@@ -273,10 +305,12 @@ def main():
         
     """
     
+    setGlobalLanguageParameters()
+    
     parser = createAParser( )  #will be used to parse options 
     
     infos = getOptionsFromParser( parser )          
-    print "prior to constructor : %s" %infos.productTypes
+    
     gp = ClientGraphicProducer( clientNames = infos.clientNames, groupName = infos.groupName , timespan = infos.timespan, currentTime = infos.currentTime, productTypes = infos.productTypes, directory = infos.directory , fileType = infos.fileType, machines = infos.machines )  
     
     gp.produceGraphicWithHourlyPickles( types = infos.types, now = infos.collectUpToNow, createCopy = infos.copy, combineClients = infos.combineClients )
