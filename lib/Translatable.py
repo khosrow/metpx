2ab7e0f61469a89733f5cf581cc4b89f7bfda72e
Nicholas Lemay <nicholaslemay@sourceforge.net>
2008-01-23 19:23:03 +0000

    Adding this mini-class to the lib folder.
    This class is to be used by classes which want 
    to specify that they derive from the Translatable 
    type. This would means they are translatable themselves
    and make it obvious to a developper which class 
    is has translatable content and which does not.

---
+++ lib/Translatable.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+"""
+#############################################################################################
+#
+#
+# Name: Translatable.py
+#
+# @author: Nicholas Lemay
+#
+# @since: 2008-01-22, last updated on  2008-01-22
+#
+#
+# @license: MetPX Copyright (C) 2004-2007  Environment Canada
+#           MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
+#           named COPYING in the root of the source directory tree.
+#
+# Description : This class is to be inherited by classes which desire to be translatable.   
+#
+# 
+#############################################################################################
+"""
+
+import sys
+
+"""
+    - Small function that adds pxStats to sys path.  
+"""
+sys.path.insert( 1, sys.path[0] + '/../../' )
+
+# These imports require pxStats.
+from pxStats.lib.LanguageTools import LanguageTools
+
+
+class Translatable:
+    
+    
+    
+    def __init__( self ):
+        raise "Class must have it's own constructor."
+        
+    
+        
+    def getTranslatorForModule(self, moduleName, language = None ):
+        """
+            @summary : Sets up all the needed global language 
+                       tranlator so that it can be used 
+                       everywhere in this program.
+            
+            @Note    : The scope of the global _ function 
+                       is restrained to this class and the one in.
+            
+            @return: None
+            
+        """
+        
+        return LanguageTools.getTranslatorForModule( moduleName, language )  
+        
+        
+        
