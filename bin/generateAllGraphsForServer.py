876a99b1a4be7b451f06f99a225946d86c9bc92c
Nicholas Lemay <nicholaslemay@sourceforge.net>
2008-02-06 21:37:37 +0000

    Added _() to string so they can be detected by pygettext.
    Added translation support.
    
    Added --outputLanguage options.
    
    Cleeaned up code somewhat.

--- bin/generateAllGraphsForServer.py
+++ bin/generateAllGraphsForServer.py
@@ -1,55 +1,57 @@
 #! /usr/bin/env python
 """
-MetPX Copyright (C) 2004-2006  Environment Canada
-MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file 
-named COPYING in the root of the source directory tree.
+
 #############################################################################################
 #
-# Name  : generateAllGraphsForServer.py
+# @name  : generateAllGraphsForServer.py
 #
-# Author: Nicholas Lemay
+# @author:  Nicholas Lemay
 #
-# Date  : 2006-09-12
+# @license: MetPX Copyright (C) 2004-2006  Environment Canada
+#           MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file 
+#           named COPYING in the root of the source directory tree.
 #
-# Description: This program is to be used to create graphics of the same timespan for 
-#              one or many machines and for all their respective clients.
+#
+# @since: 2006-09-12 last updated on 2007-07-17
+#
+# @summary: This program is to be used to create graphics of the same timespan for 
+#           one or many machines and for all their respective clients.
 #                           
-#              Graphics can also be produce by merging the data from different machines.
+#           Graphics can also be produce by merging the data from different machines.
 #
-# Usage:   This program can be called from command-line. Use -h for usage.
+# @Usage:   This program can be called from command-line. Use -h for usage.
 #
 #
 ##############################################################################################
 """
 
-import os, time, pwd, sys, getopt, commands, fnmatch, pickle
+import os, time, pwd, sys, getopt, commands, fnmatch, gettext, pickle
+from   optparse  import OptionParser
+
+
 """
     Small function that adds pxlib to the environment path.  
 """
 sys.path.insert(1, sys.path[0] + '/../../')
-try:
-    pxlib = os.path.normpath( os.environ['PXROOT'] ) + '/lib/'
-except KeyError:
-    pxlib = '/apps/px/lib/'
-sys.path.append(pxlib)
-
-
-
+from pxStats.lib.StatsPaths import StatsPaths
+from pxStats.lib.StatsDateLib import StatsDateLib
+from pxStats.lib.GeneralStatsLibraryMethods import GeneralStatsLibraryMethods
+from pxStats.lib.LanguageTools import LanguageTools
 
 """
     Imports
     PXManager requires pxlib 
 """
-from optparse  import OptionParser
+STATSPATHS = StatsPaths( )
+STATSPATHS.setPaths( LanguageTools.getMainApplicationLanguage() )
+sys.path.append( STATSPATHS.PXLIB )
+
+
 from PXManager import *
-
-from pxStats.lib.StatsPaths import StatsPaths
-from pxStats.lib.StatsDateLib import StatsDateLib
-from pxStats.lib.GeneralStatsLibraryMethods import GeneralStatsLibraryMethods
-
 
 
 LOCAL_MACHINE  = os.uname()[1]
+CURRENT_MODULE_ABS_PATH = os.path.abspath( sys.path[0] ) + '/' + __name__ 
 
 #################################################################
 #                                                               #
@@ -58,10 +60,26 @@ LOCAL_MACHINE  = os.uname()[1]
 ################################################################# 
 class _Infos:
 
-    def __init__( self, date, machines, timespan, logins, combinedName, combine, individual ):
+    def __init__( self, date, machines, timespan, logins, combinedName, combine, individual, outputLanguage ):
         """
             Data structure to be used to store parameters within parser.
-        
+             
+            @param date: Date of the call
+            
+            @param machines: Machines for which to produce de graphics 
+            
+            @param timespan: Span for which to produce the graphics
+            
+            @param logins: Logins to the machine we need to connect to.
+            
+            @param combinedName: Combined name of the machines.
+            
+            @param combine:Whether or not to combine the data of the specified machines .
+            
+            @param individual: Whether or not to combine the data of the specified machines.
+            
+            @param outputLanguage: Language in which the graphics will be outputted.
+                  
         """
                     
         self.logins      = logins       # Logins for all machines. 
@@ -71,16 +89,17 @@ class _Infos:
         self.combinedName= combinedName # To be used if merges = True.
         self.combine     = combine      # Whether the machines passed in parameter need to be combined to create graphs
         self.individual  = individual   # Whether we create non combined graphs for different machines or not.
-
+        self.outputLanguage = outputLanguage # Language outputted by the created graphics.
         
+
         
 def createParser( ):
     """ 
-        Builds and returns the parser 
+        @summary : Builds and returns the parser 
     
     """
     
-    usage = """
+    usage = _( """
 
 %prog [options]
 ********************************************
@@ -104,7 +123,7 @@ Options:
     - With -l|--logins you can specify wich login must be used for each of the enumerated machines.
     - With -m|--machines you can specify the list of machines to be used.
     - With -s|--span you can specify the time span to be used to create the graphic     
-      
+    - With -o|--outputLanguage you can specify the languiage displayed on the graphics.
             
 Ex1: %prog                                   --> All default values will be used. Not recommended.
 Ex2: %prog -i -c -m "m1,m2" -l "l1,l2" -s 24 --> Generate graphs for all clients found on m1 and m2.
@@ -116,7 +135,7 @@ Ex2: %prog -i -c -m "m1,m2" -l "l1,l2" -s 24 --> Generate graphs for all clients
  
 ********************************************
 * See /doc.txt for more details.           *
-********************************************"""   
+********************************************"""   )
     
     parser = OptionParser( usage )
     addOptions( parser )
@@ -127,33 +146,42 @@ Ex2: %prog -i -c -m "m1,m2" -l "l1,l2" -s 24 --> Generate graphs for all clients
 
 def addOptions( parser ):
     """
-        This method is used to add all available options to the option parser.
+        @summary : This method is used to add all available options to the option parser.
         
     """
     
-    parser.add_option("-c", "--combine", action="store_true", dest = "combine", default=False, help="Combine data from all specified machines.")
+    parser.add_option("-c", "--combine", action="store_true", dest = "combine", default=False, \
+                      help=_("Combine data from all specified machines.") )
     
-    parser.add_option("-d", "--date", action="store", type="string", dest="date", default=StatsDateLib.getIsoFromEpoch( time.time() ), help="Decide current time. Usefull for testing.") 
+    parser.add_option("-d", "--date", action="store", type="string", dest="date", default=StatsDateLib.getIsoFromEpoch( time.time() ),\
+                       help=_("Decide current time. Usefull for testing.") ) 
     
-    parser.add_option("-i", "--individual", action="store_true", dest = "individual", default=False, help="Create individual graphics for all specified machines.")                    
+    parser.add_option("-i", "--individual", action="store_true", dest = "individual", default=False, \
+                      help=_("Create individual graphics for all specified machines.") )                    
     
-    parser.add_option( "-l", "--logins", action="store", type="string", dest="logins", default="pds", help = "Logins to be used to connect to machines." ) 
+    parser.add_option( "-l", "--logins", action="store", type="string", dest="logins", default="pds",\
+                        help = _("Logins to be used to connect to machines.") ) 
     
-    parser.add_option( "-m", "--machines", action="store", type="string", dest="machines", default=LOCAL_MACHINE, help = "Machines for wich you want to collect data." ) 
+    parser.add_option( "-m", "--machines", action="store", type="string", dest="machines", default=LOCAL_MACHINE,\
+                        help = _("Machines for wich you want to collect data.") ) 
     
-    parser.add_option("-s", "--span", action="store",type ="int", dest = "timespan", default=24, help="timespan( in hours) of the graphic.")    
+    parser.add_option("-o", "--outputLanguage", action="store", type="string", dest="outputLanguage",\
+                       default=LanguageTools.getMainApplicationLanguage(), help = _("Language in which the graphics are outputted.") ) 
+    
+    parser.add_option("-s", "--span", action="store",type ="int", dest = "timespan", default=24, \
+                      help=_("timespan( in hours) of the graphic."))    
     
     
   
 def getOptionsFromParser( parser ):
     """
         
-        This method parses the argv received when the program was called
-        It takes the params wich have been passed by the user and sets them 
-        in the corresponding fields of the infos variable.   
+        @summary : This method parses the argv received when the program was called
+                   It takes the params wich have been passed by the user and sets them 
+                   in the corresponding fields of the infos variable.   
     
-        If errors are encountered in parameters used, it will immediatly terminate 
-        the application. 
+                   If errors are encountered in parameters used, it will immediatly terminate 
+                   the application. 
     
     """    
         
@@ -165,7 +193,7 @@ def getOptionsFromParser( parser ):
     logins           = options.logins.replace( '"', '' ).replace( " ","" ).split( ',' )     
     combine          = options.combine
     individual       = options.individual
-    
+    outputLanguage   = options.outputLanguage
     
     
     try: # Makes sure date is of valid format. 
@@ -175,9 +203,9 @@ def getOptionsFromParser( parser ):
         currentTime = "%s %s" %( split[0], split[1] )
 
     except:    
-        print "Error. The date format must be YYYY-MM-DD HH:MM:SS" 
-        print "Use -h for help."
-        print "Program terminated."
+        print _( "Error. The date format must be YYYY-MM-DD HH:MM:SS" )
+        print _( "Use -h for help." )
+        print _( "Program terminated." )
         sys.exit()
     
     
@@ -187,16 +215,16 @@ def getOptionsFromParser( parser ):
                 
     except:
         
-        print "Error. The timespan value needs to be an integer one above 0." 
-        print "Use -h for help."
-        print "Program terminated."
+        print _( "Error. The timespan value needs to be an integer one above 0." )
+        print _( "Use -h for help." )
+        print _( "Program terminated." )
         sys.exit()    
                   
         
     if len( logins ) != len( machines ) :
-        print "Error. Number of logins does not match number of machines." 
-        print "Use -l 'login1,login2,loginX' for multiple machines."
-        print "Program terminated."         
+        print _( "Error. Number of logins does not match number of machines." )
+        print _( "Use -l 'login1,login2,loginX' for multiple machines." )
+        print _( "Program terminated." )         
         sys.exit()
     
    
@@ -207,7 +235,9 @@ def getOptionsFromParser( parser ):
     elif combine == False and individual == False :#no option specified + len >1      
         combine = True    
         
-    infos = _Infos( date = date, machines = machines, timespan = timespan, logins = logins, combine = combine, individual = individual, combinedName = combinedName  )   
+    infos = _Infos( date = date, machines = machines, timespan = timespan, logins = logins,\
+                    combine = combine, individual = individual, combinedName = combinedName,\
+                    outputLanguage = outputLanguage  )   
     
     return infos     
 
@@ -220,8 +250,8 @@ def getOptionsFromParser( parser ):
 #################################################################     
 def generateGraphsForIndividualMachines( infos ) :
     """
-        Generate graphs for every specified machine withoout
-        merging any of the data between the machines.  
+        @summary : Generate graphs for every specified machine withoout
+                   merging any of the data between the machines.  
           
     """       
              
@@ -233,7 +263,8 @@ def generateGraphsForIndividualMachines( infos ) :
             pid = os.fork()#create child process
             
             if pid == 0: #child process
-                status, output = commands.getstatusoutput( "python %sgenerateGraphics.py -m '%s' -f tx -c '%s' -d '%s' -s %s --copy" %( StatsPaths.STATSBIN, infos.machines[i], txName, infos.date, infos.timespan) )               
+                output = commands.getoutput( "python %sgenerateGraphics.py -m '%s' -f tx -c '%s' -d '%s' -s %s --copy --outputLanguage %s" \
+                                             %( StatsPaths.STATSBIN, infos.machines[i], txName, infos.date, infos.timespan, infos.outputLanguage  ) )               
                 print output 
                 sys.exit()
         
@@ -259,7 +290,8 @@ def generateGraphsForIndividualMachines( infos ) :
             pid = os.fork()#create child process
             
             if pid == 0 :#child process
-                status, output = commands.getstatusoutput( "python %sgenerateGraphics.py -m '%s' -f rx -c '%s' -d '%s' -s %s --copy" %( StatsPaths.STATSBIN, infos.machines[i] , rxName, infos.date,infos.timespan ) )     
+                output = commands.getoutput( "python %sgenerateGraphics.py -m '%s' -f rx -c '%s' -d '%s' -s %s --copy --outputLanguage %s"\
+                                             %( StatsPaths.STATSBIN, infos.machines[i] , rxName, infos.date,infos.timespan, infos.outputLanguage ) )     
                 print output
                 sys.exit()
         
@@ -281,7 +313,9 @@ def generateGraphsForIndividualMachines( infos ) :
                 
 def generateGraphsForPairedMachines( infos ) :
     """
-        Create graphs for all client by merging the data from all the listed machines.    
+        @summary : Create graphs for all client 
+                   by merging the data from all
+                   the listed machines.    
     
     """        
     
@@ -298,7 +332,8 @@ def generateGraphsForPairedMachines( infos ) :
         
         if pid == 0 :#child process
             
-            status, output = commands.getstatusoutput( "python %sgenerateGraphics.py -m %s -f tx -c %s -d '%s' -s %s  --copy" %( StatsPaths.STATSBIN, infos.combinedName, txName, infos.date, infos.timespan ) )
+            output = commands.getoutput( "python %sgenerateGraphics.py -m %s -f tx -c %s -d '%s' -s %s  --copy --outputLanguage %s"
+                                         %( StatsPaths.STATSBIN, infos.combinedName, txName, infos.date, infos.timespan, infos.outputLanguage ) )
             print output
             sys.exit()    #terminate child process
     
@@ -308,7 +343,7 @@ def generateGraphsForPairedMachines( infos ) :
             if j %10 == 0:
                 while True:#wait on all non terminated child process'
                     try:   #will raise exception when no child process remain.
-                        pid, status = os.wait( )
+                        pid, status = os.wait( ) 
                     except:
                         break
                                                                                                                                                                         
@@ -324,7 +359,8 @@ def generateGraphsForPairedMachines( infos ) :
         pid = os.fork()#create child process
         
         if pid == 0:#child process            
-            status, output = commands.getstatusoutput( "python %sgenerateGraphics.py -m %s -f rx -c %s -d '%s' -s %s  --copy" %( StatsPaths.STATSBIN, infos.combinedName, rxName, infos.date, infos.timespan ) )     
+            output = commands.getoutput( "python %sgenerateGraphics.py -m %s -f rx -c %s -d '%s' -s %s  --copy --outputLanguage %s"\
+                                        %( StatsPaths.STATSBIN, infos.combinedName, rxName, infos.date, infos.timespan, infos.outputLanguage ) )     
             print output 
             sys.exit()
         else:
@@ -344,15 +380,39 @@ def generateGraphsForPairedMachines( infos ) :
         except:    
             break  
    
+   
+   
+def  setGlobalLanguageParameters():
+    """
+        @summary : Sets up all the needed global language 
+                   tranlator so that it can be used 
+                   everywhere in this program.
+        
+        @Note    : The scope of the global _ function 
+                   is restrained to this module only and
+                   does not cover the entire project.
+        
+        @return: None
+        
+    """
+    
+    global _ 
+    
+    _ = LanguageTools.getTranslatorForModule( CURRENT_MODULE_ABS_PATH )     
+   
+   
     
 def main():
     """
         
-        Create graphics of the same timespan for 
-        one or many machines and for all their respective clients.
+        @summary : Create graphics of the same timespan for 
+                   one or many machines and for all their
+                   respective clients.
     
     """    
-
+    
+    setGlobalLanguageParameters()
+    
     parser = createParser( )  #will be used to parse options 
     
     infos = getOptionsFromParser( parser )    
