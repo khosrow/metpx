638c6845205adb01d162e591f6b7ca5c64c01382
Nicholas Lemay <nicholaslemay@sourceforge.net>
2008-03-11 18:57:57 +0000

    Added support for translations.

--- pxStats/bin/tools/fileRenamer.py
+++ pxStats/bin/tools/fileRenamer.py
@@ -28,31 +28,71 @@ named COPYING in the root of the source directory tree.
   
 """ 
 
-import commands, fnmatch, os, pickle, sys  
+import commands, os, sys  
 sys.path.insert(1, sys.path[0] + '/../../../')
 
 from   optparse import OptionParser  
 from fnmatch import fnmatch  
 from pxStats.lib.StatsPaths import StatsPaths
+from pxStats.lib.LanguageTools import LanguageTools
 
 LOCAL_MACHINE = os.uname()[1]
+CURRENT_MODULE_ABS_PATH = os.path.abspath( sys.path[0] ) + '/' + "fileRenamer.py" 
+
+
+class Parameters:
+    
+    def __init__( self, clientNames, groupNames, machineNames, overrideConfirmation, newValue, oldValue ):
+        """
+        
+            @param clientNames: Whether the change affects the client names or not. 
+            
+            @param groupNames:  Whether the change affects the group names or not.
+            
+            @param machineNames : Whether the change affects the machine names or not.
+            
+            @param overrideConfirmation: 
+            
+            @param newValue: Old value that needs to be changes.
+            
+            @param oldValue: New value that will replace the old value.
+            
+        """
+        
+        self.clientNames = clientNames
+        self.groupNames = groupNames
+        self.machineNames = machineNames
+        self.overrideConfirmation = overrideConfirmation
+        self.newValue = newValue
+        self.oldValue = oldValue
+
 
 
 def getOptionsFromParser( parser ):
     """
         
-        This method parses the argv received when the program was called
-        and returns the parameters.    
- 
+        @summary : This method parses the argv 
+                   received when the program 
+                   was called and returns the
+                   parameters.    
+        
+        @return : Options instance containing
+                  the parsed values.
+    
     """ 
 
     
-    ( options, args )    = parser.parse_args()       
+    ( options, args )    = parser.parse_args()     
+      
+    clientNames          = options.clientNames
+    groupNames           = options.groupNames  
     overrideConfirmation = options.overrideConfirmation   
-    oldMachineName       = options.oldMachineName.replace( " ","" )
-    newMachineName       = options.newMachineName.replace( ' ','' )
+    oldValue             = options.oldValue.replace( " ","" )
+    newValue             = options.newValue.replace( ' ','' )
+
+    newOptions = Parameters( clientNames, groupNames, overrideConfirmation, oldValue, newValue )
            
-    return oldMachineName, newMachineName, overrideConfirmation 
+    return newOptions
 
     
     
@@ -62,7 +102,7 @@ def createParser( ):
     
     """
     
-    usage = """
+    usage = _( """
 
 %prog [options]
 ********************************************
@@ -78,8 +118,8 @@ Defaults :
 Options:
     
     - With -h|--help you can get help on this program. 
-    - With -n|--newMachineName you can specify the name of the new machine.
-    - With -o|--oldMachineName you can specify the name of the old machine.     
+    - With -n|--newValue you can specify the name of the new machine.
+    - With -o|--oldValue you can specify the name of the old machine.     
     - With --overrideConfirmation you can specify that you want to override the confirmation request.
          
             
@@ -89,7 +129,7 @@ Ex3: %prog -o 'm1' -n 'm2' --overrideConfirmation --> M1 to m2, no confirmations
 
 ********************************************
 * See /doc.txt for more details.           *
-********************************************"""
+********************************************""" )
     
     parser = OptionParser( usage )
     addOptions( parser )
@@ -105,14 +145,59 @@ def addOptions( parser ):
         @param parser: parser to wich the options need to be added. 
     
     """
+    parser.add_option( "-c", "--clientNames", action="store_true", dest = "clientNames", default=False, help= _( "Use if you want to rename files based on a change of client names." ) )
     
-    parser.add_option( "-o", "--oldMachineName", action="store", type="string", dest="oldMachineName", default="",
-                        help="Name of the old machine." )             
+    parser.add_option( "-g", "--groupNames", action="store_true", dest = "groupNames", default=False, help= _( "Use if you want to rename files based on a change of group names." ) )
+    
+    parser.add_option( "-m", "--machineNames", action="store_true", dest = "machineNames", default=False, help= _( "Use if you want to rename files based on a change of machine names.") )
+    
+    parser.add_option( "-o", "--oldValue", action="store", type="string", dest="oldMachineName", default="",
+                        help=_( "Name of the old machine." ) )             
          
-    parser.add_option( "-n", "--newMachineName", action="store", type="string", dest="newMachineName", default="",  help="Name of the new machine name." ) 
+    parser.add_option( "-n", "--newValue", action="store", type="string", dest="newMachineName", default="",  help=_( "Name of the new machine name.") ) 
           
-    parser.add_option( "--overrideConfirmation", action="store_true", dest = "overrideConfirmation", default=False, help="Whether or not to override the confirmation request." )
+    parser.add_option( "--overrideConfirmation", action="store_true", dest = "overrideConfirmation", default=False, help=_( "Whether or not to override the confirmation request." ) )
+
     
+
+def validateParameters( parameters ):
+    """
+        @summary          : Validates the content of the Parameters() instance.
+        
+        @param parameters: Parameters() instance containing 
+                           the values chosen by the user.
+                           
+        @note : If illegal use of parameters is found,
+                application will be terminated.
+    """
+    
+    parameters = Parameters()
+    
+    if parameters.clientNames  == False and parameters.groupNames == False and parameters.machineNames == False :
+        print _( "Error. You need to choose what kind of change needs to be made." )
+        print _( "Please select between clientNames, groupNames and machineName. Use -h for further help." )
+        print _( "Program terminated." )
+        sys.exit()
+    
+    elif (parameters.clientNames ^ parameters.groupNames ^ parameters.machineNames )  == False :
+        print _( "Error. You can only select a single kind of change to be made." )
+        print _( "Please select between clientNames, groupNames and machineName. Use -h for further help." )
+        print _( "Program terminated." )
+        sys.exit()
+        
+    elif parameters.newValue == "" or parameters.oldValue == "":
+        print _( "Error. You need to specify both a newValue and an oldValue." )
+        print _( "Please use the --newValue and --oldValue options. Use -h for further help." )
+        print _( "Program terminated." )
+        sys.exit()
+    
+    elif parameters.newValue  == parameters.oldValue : 
+        print _( "Error. The new value needs to be different from the old value." )
+        print _( "Please make sure values specified with the --newValue and --oldValue options are different. Use -h for further help." )
+        print _( "Program terminated." )
+        sys.exit()             
+ 
+ 
  
 def filterentriesStartingWithDots(x):
     """
@@ -121,7 +206,7 @@ def filterentriesStartingWithDots(x):
         starting with a dot.
     """
     
-    return not fnmatch(x,".*")
+    return not fnmatch( x, ".*" )
 
 
 
@@ -310,7 +395,7 @@ def renamePickles( oldMachineName, newMachineName ):
         
     
     
-def  renameFileVersions( oldMachineName, newMachineName ):  
+def renameFileVersions( oldMachineName, newMachineName ):  
     """
         @summary: Renames all the file version files sporting a certain machine name's( oldMachineName ) 
                   so that they now sport the name of another machine(newMachineName). 
@@ -334,6 +419,24 @@ def  renameFileVersions( oldMachineName, newMachineName ):
                 #print "mv %s %s " %( source, destination )                          
                 status, output = commands.getstatusoutput( "mv %s %s" %(source,destination) )  
     
+    
+    
+def renameGroupInConfigFile( oldName, newName ):
+    """
+    """
+    fileHandle = open( open( StatsPaths.STATSETC + 'config' ), "r" )
+    linesFromConfigFile = fileHandle.readlines()
+    for i in range( len( linesFromConfigFile ) ):
+        name = ( linesFromConfigFile[i].split( "=" )[0] ).replace( " ", "" )
+        if name == oldName:
+            linesFromConfigFile[i] = linesFromConfigFile[i].replace( oldName, newName )    
+        break    
+    fileHandle.close()
+    
+    fileHandle = open(  open( StatsPaths.STATSETC + 'config' ), "w" )
+    fileHandle.writelines( linesFromConfigFile )    
+    fileHandle.close()
+
 
 
 def getConfirmation( oldMachineName, newMachineName ):
@@ -353,7 +456,7 @@ def getConfirmation( oldMachineName, newMachineName ):
     confirmation = False
     os.system( 'clear' )
     
-    print """
+    print _( """
     ###########################################################
     #  pickleRenamer.py                                       #
     #  MetPX Copyright (C) 2004-2006  Environment Canada      #
@@ -361,22 +464,86 @@ def getConfirmation( oldMachineName, newMachineName ):
 
     
 
-    """
-    question = "Are you sure you want to rename all %s file to %s files (y or n) ?  " %(oldMachineName, newMachineName)
+    """ )
+    question = _( "Are you sure you want to rename all %s file to %s files (y or n) ?  ") %(oldMachineName, newMachineName)
     answer = raw_input( question  ).replace(' ','').replace( '\n','')
     answer = answer.lower()
     
-    while( answer != 'y' and answer != 'n'):
-        print "Error. You must either enter y or n."     
+    while( answer != _('y') and answer != _('n') ):
+        print _("Error. You must either enter y or n.")     
         answer = raw_input( question )
         
-    if answer == 'y':
+    if answer == _('y'):
         confirmation = True
     
     return confirmation 
    
+   
         
-      
+def doRenamingForClients( parameters ):
+    """
+        @summary : Renames files based on client names.
+        
+        @param parameters: Parameters with whom this program was called.
+        
+        @return : None          
+    """
+    
+    #renameFileVersions()
+    #renamePickles()
+    #renameDatabases()
+    
+    x =2 
+
+
+
+def doRenamingForGroups( parameters ):
+    """  
+        @summary : Renames files based on group names.
+        
+        @param parameters: Parameters with whom this program was called.
+        
+        @return : None   
+       
+    """
+    
+    x = 2 
+    
+    
+def doRenamingForMachines( parameters ):
+    """    
+        @summary : Renames files based on machine names.
+        
+        @param parameters: Parameters with whom this program was called.
+        
+        @return : None 
+         
+    """
+    
+    x =2 
+    
+    
+    
+def  setGlobalLanguageParameters():
+    """
+        @summary : Sets up all the needed global language 
+                   tranlator so that it can be used 
+                   everywhere in this program.
+        
+        @Note    : The scope of the global _ function 
+                   is restrained to this module only and
+                   does not cover the entire project.
+        
+        @return: None
+        
+    """
+    
+    global _ 
+    
+    _ = LanguageTools.getTranslatorForModule( CURRENT_MODULE_ABS_PATH )    
+    
+    
+    
 def main():    
     """
         
@@ -387,22 +554,32 @@ def main():
                     
         
     """ 
+    
+    setGlobalLanguageParameters()
+    
     manualConfirmation = False 
     overrideConfirmation = False
+    
     parser   = createParser( )  #will be used to parse options 
-    oldMachineName, newMachineName, overrideConfirmation = getOptionsFromParser( parser )
+    parameters = getOptionsFromParser( parser )
+    validateParameters( parameters )
+    
     
     
     if overrideConfirmation == False:
-        manualConfirmation = getConfirmation( oldMachineName, newMachineName )
+        manualConfirmation = getConfirmation( parameters )
     
-    if overrideConfirmation ==True or manualConfirmation == True:    
-        renamePickles( oldMachineName, newMachineName )
-        renameDatabases( oldMachineName, newMachineName )
-        renameFileVersions( oldMachineName, newMachineName )
-   
+    if overrideConfirmation == True or manualConfirmation == True:
+        
+        if parameters.clientNames == True :
+            doRenamingForClients()
+        elif parameters.groupNames == True :
+            doRenamingForGroups()    
+        else :
+            doRenamingForMachines()
+                
     else:
-        print "Program terminated." 
+        print _("Program terminated.")
         
 
 if __name__ == "__main__":
