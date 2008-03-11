6b4037090afb2928dca8bb450dce7d2d46f5ceb9
Nicholas Lemay <nicholaslemay@sourceforge.net>
2008-03-11 18:16:33 +0000

    Added translation support.

--- bin/debugTools/setTimeOfLastUpdates.py
+++ bin/debugTools/setTimeOfLastUpdates.py
@@ -1,20 +1,20 @@
 #! /usr/bin/env python
 """
-MetPX Copyright (C) 2004-2006  Environment Canada
-MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file 
-named COPYING in the root of the source directory tree.
-
 #############################################################################################
-# Name  : pickleCleaner.py
+# @summary:   : setTimeOfLastUpdates.py
 #
-# Author: Nicholas Lemay
+# @author     : Nicholas Lemay
 #
-# Date  : 2006-10-12
+# @since  : 2006-10-12, last updates on March 11th 2008
 #
-# Description: This program is to be used in case of a problem with pickle/updater to set back 
+# @summary:    This program is to be used in case of a problem with pickle/updater to set back 
 #              the time of the the last updates prior to the errors. That way at the next 
 #              update the pickling will be redone and might produce the right pickles if 
 #              the problem was corrected. 
+#
+# @license :  MetPX Copyright (C) 2004-2006  Environment Canada
+#             MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file 
+#             named COPYING in the root of the source directory tree. 
 #
 #   Usage:   This program can be called from a crontab or from command-line. 
 #
@@ -29,53 +29,52 @@ sys.path.insert(1, sys.path[0] + '/../../../')
 
 from pxStats.lib.StatsPaths import StatsPaths
 from pxStats.lib.StatsDateLib import StatsDateLib
+from pxStats.lib.LanguageTools import LanguageTools
 
+
+TOTAL_YEARLY_OPERATIONAL_COSTS =  10
+CURRENT_MODULE_ABS_PATH = os.path.abspath( sys.path[0] ) + '/' + "setTimeOfLastUpdates.py" 
 
 
 def updatePickledTimes( dateToSet = "2006-10-23 09:00:00"  ):
     """
-          Get all the keys then set all of them to the desired date.
+          @summary : Get all the keys then set all of them to the desired date.
     """
     
-    fileName = StatsPaths.STATSROOT +"PICKLED-TIMES"
-
-    if os.path.isfile( fileName ):
-
-        fileHandle   = open( fileName, "r" )
-        pickledTimes = pickle.load( fileHandle )
-        fileHandle.close()
-        
-        
-        keys = pickledTimes.keys()
-        for key in keys:
-            pickledTimes[key] = dateToSet
+    folder = StatsPaths.STATSPICKLESTIMEOFUPDATES
+    
+    files = os.listdir(folder)
+    for fileName in files :
+        if os.path.isfile( fileName ):
+    
+            fileHandle   = open( fileName, "r" )
+            pickledTimes = pickle.load( fileHandle )
+            fileHandle.close()
             
-        fileHandle  = open( fileName, "w" )
-
-        pickle.dump( pickledTimes, fileHandle )
-
-        fileHandle.close()
-
+            
+            keys = pickledTimes.keys()
+            for key in keys:
+                pickledTimes[key] = dateToSet
+                
+            fileHandle  = open( fileName, "w" )
+    
+            pickle.dump( pickledTimes, fileHandle )
+    
+            fileHandle.close()
+    
 
 
     
-def removePickledFilePositions(  ):
-    """
-        Deletes pickle file containing the position where we last read 
-        a certain file for each source/client.
-    """
-    
-    if os.path.isfile( StatsPaths.STATSROOT +  "PICKLED_FILE_POSITIONS" ) :
-        status, output = commands.getstatusoutput("rm %s " %( StatsPaths.STATSROOT +  "PICKLED_FILE_POSITIONS" ) )
+
                 
     
     
 def main():
     """
-        Deletes pickle file containing the position where we last read 
-        a certain file for each source/client.
+        @summary : Deletes pickle file containing the position where we last read 
+                   a certain file for each source/client.
         
-        Set date of last update in StatsPaths.STATSROOT +"PICKLED-TIMES"
+                   Set date of last update in StatsPaths.STATSROOT +"PICKLED-TIMES"
         
     """
     
@@ -89,19 +88,18 @@ def main():
             t =  time.strptime( dateToSet, '%Y-%m-%d %H:%M:%S' )#will raise exception if format is wrong.
             split = dateToSet.split()
             dateToSet = "%s %s" %( split[0], split[1] )
-            removePickledFilePositions()
             updatePickledTimes( dateToSet )
         
         except:
-            print 'Date must be of the following format "YYYY-MM-DD HH:MM:SS"'
-            print "Program terminated."     
+            print _( 'Date must be of the following format "YYYY-MM-DD HH:MM:SS"')
+            print _( "Program terminated." )
             sys.exit()
                 
     
     else:
-        print "You must specify date to set."
-        print "Date must be of the folowing format YYYY-MM-DD HH:MM:SS"
-        print "Program terminated."        
+        print _( "You must specify date to set." )
+        print _( "Date must be of the folowing format YYYY-MM-DD HH:MM:SS" )
+        print _( "Program terminated."         )
             
 
 
