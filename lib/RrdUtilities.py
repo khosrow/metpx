b80e2d59a39e59d5f77963ae84225501ee397c86
Nicholas Lemay <nicholaslemay@sourceforge.net>
2008-03-20 13:43:21 +0000

    Added proper use of statspaths.
    Added proper addition of PXLIB to 
    sys.path.

--- lib/RrdUtilities.py
+++ lib/RrdUtilities.py
@@ -1,50 +1,59 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 """
-MetPX Copyright (C) 2004-2006  Environment Canada
-MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file 
-named COPYING in the root of the source directory tree.
-
 #############################################################################################
-# Name  : rrdUtilities.py
+# @name   : RrdUtilities.py
 #
-# Author: Nicholas Lemay
+# @author : Nicholas Lemay
 #
-# Date  : 2007-05-08
+# @since  : 2007-05-08, last updated on March 20th 2008
 #
-# Description: This file containsmethods that are usefull to all programs that deal with 
+# @license : MetPX Copyright (C) 2004-2006  Environment Canada
+#            MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file 
+#            named COPYING in the root of the source directory tree.
+#
+#
+# @summary   : This file containsmethods that are usefull to all programs that deal with 
 #              round robin databases. They have been gathered here as to limit repetition
 #              and hopefully make program maintenance easier.
 #
 ##############################################################################################
 """
 
-import os, sys
+import os, pickle, sys
+
+"""
+    Small function that adds pxStats to the environment path.  
+"""
+sys.path.insert(1, sys.path[0] + '/../../')
+from   pxStats.lib.StatsPaths   import StatsPaths
+
 
 """
     Small function that adds pxlib to the environment path.  
 """
-sys.path.insert(1, sys.path[0] + '/../../')
-try:
-    pxlib = os.path.normpath( os.environ['PXROOT'] ) + '/lib/'
-except KeyError:
-    pxlib = '/apps/px/lib/'
-sys.path.append(pxlib)
-
+STATSPATHS = StatsPaths()
+STATSPATHS.setPaths()
+sys.path.append( STATSPATHS.PXLIB )
 
 """
     Imports
     PxManager requires pxlib 
 """
 from   PXManager    import *        
-from   pxStats.lib.StatsPaths   import StatsPaths
-          
 
 LOCAL_MACHINE = os.uname()[1]  
 
+
+
 class RrdUtilities:          
     
-    def buildRRDFileName( dataType = 'errors', clients = ['client1','client1'] , machines = ['machine1','machine2'], groupName = "", fileType = "", usage = "regular"  ):
+    
+    
+    def buildRRDFileName( dataType = 'errors', clients = ['client1','client1'] , machines = ['machine1','machine2'],\
+                          groupName = "", fileType = "", usage = "regular"  ):
         """
+            @summary : Returns the name of the round robin database bases on the parameters.
         
             @param dataType: byteocunt, errors, filecount, filesOverMaxLatency and latency.
             
@@ -75,19 +84,23 @@ class RrdUtilities:
         
         if len(clients) ==1:       
             if usage == "regular":
-                fileName = StatsPaths.STATSCURRENTDB + "%s/%s_%s" %( dataType, combinedClientsName, combinedMachineName )  
+                fileName = STATSPATHS.STATSCURRENTDB + "%s/%s_%s" %( dataType, combinedClientsName, combinedMachineName )  
             elif usage == "group":
-                 fileName = StatsPaths.STATSCURRENTDB + "%s/%s_%s" %( dataType, groupName, combinedMachineName )    
+                 fileName = STATSPATHS.STATSCURRENTDB + "%s/%s_%s" %( dataType, groupName, combinedMachineName )    
             elif usage == "totalForMachine":
                  fileName = StatsPaths.STATSCURRENTDB + "%s/%s_%s" %( dataType, fileType, combinedMachineName )            
         else:
             if usage == "regular":
-                fileName = StatsPaths.STATSCURRENTDB + "%s/combined/%s_%s" %( dataType, combinedClientsName, combinedMachineName )  
+                fileName = STATSPATHS.STATSCURRENTDB + "%s/combined/%s_%s" %( dataType, combinedClientsName, combinedMachineName )  
             elif usage == "group":
-                 fileName = StatsPaths.STATSCURRENTDB + "%s/combined/%s_%s" %( dataType, groupName, combinedMachineName )    
+                 fileName = STATSPATHS.STATSCURRENTDB + "%s/combined/%s_%s" %( dataType, groupName, combinedMachineName )    
             elif usage == "totalForMachine":
-                 fileName = StatsPaths.STATSCURRENTDB + "%s/combined/%s_%s" %( dataType, fileType, combinedMachineName )   
-            
+                 fileName = STATSPATHS.STATSCURRENTDB + "%s/combined/%s_%s" %( dataType, fileType, combinedMachineName )   
+        
+        #print "before ", fileName 
+        #fileName = fileName.replace("année","year").replace("nbreDeBytes","bytecount").replace("nbreDeFichiers","filecount").replace("erreurs","errors").replace("latence","latency").replace("fichiersAvecLatenceInnacceptable","filesOverMaxLatency").replace("heures","hours").replace("mois","month").replace("jour","day").replace("","")    
+        #print "after ", fileName
+        
         return  fileName 
     
     
@@ -97,14 +110,23 @@ class RrdUtilities:
     
     def setDatabaseTimeOfUpdate(  databaseName, fileType, timeOfUpdate ):
         """
-            This method set the time of the last update made on the database.
+            @summary : This method set the time of the last 
+                       update made on the database.
             
-            Usefull for testing. Round Robin Databae cannot be updates with 
-            dates prior to the date of the last update.
+            @Note : Usefull for automated updates. 
+                    Also usefull for testing. 
+            
+            @warning : Round Robin Databae cannot be updated with 
+                       dates prior to the date of the last update 
+                       contained within the database. 
+                       
+                       Using this method to set a date prior thos that
+                       date will only cause useless errors.
+                       
             
         """      
          
-        folder   = StatsPaths.STATSCURRENTDBUPDATES + "%s/" %fileType
+        folder   = STATSPATHS.STATSCURRENTDBUPDATES + "%s/" %fileType
         fileName = folder +  os.path.basename(databaseName)   
         if not os.path.isdir( folder ):
             os.makedirs( folder )
@@ -121,15 +143,21 @@ class RrdUtilities:
     
     def getDatabaseTimeOfUpdate( databaseName, fileType ):
         """
-            If present in DATABASE-UPDATES file, returns the time of the last 
-            update associated with the databse name.      
+            @summary : If file containging the last update exists,
+                       returns the time of the last update associated
+                       with the database name.      
             
-            Otherwise returns 0 
+            @param databaseName: Name of the database for which the time 
+                                 of the last update is needed.
+            
+            @param fileType: TX or RX 
+             
+            @return : Returns the time of the last update. Otherwise returns 0 
             
         """ 
         
         lastUpdate = 0
-        folder   = StatsPaths.STATSCURRENTDBUPDATES + "%s/" %(fileType )    
+        folder   = STATSPATHS.STATSCURRENTDBUPDATES + "%s/" %(fileType )    
         fileName = folder + os.path.basename( databaseName )
         
         if os.path.isfile( fileName ):
@@ -144,3 +172,44 @@ class RrdUtilities:
     getDatabaseTimeOfUpdate = staticmethod( getDatabaseTimeOfUpdate )
     
     
+    def getMostPopularTimeOfLastUpdate( dataBaseNames, fileType = "tx" ):
+        """
+            @summary : Gathers the time of the last 
+                       update of all the specified 
+                       databases and returns the 
+                       update time that was found
+                       to be the most popular.
+           
+           
+           @param dataBaseNames: list of database names 
+                                 for which we are interested 
+                                 in finding their update times.
+           
+           @param fileType : FileType associated with the databases.   
+                                 
+           @return: Return the most popular time of update found.
+                                                
+                         
+        """
+        
+        maximumPopularity = 0
+        mostPopular = 0
+        timesOfUpdate = {}
+        
+        
+        for dataBaseName in dataBaseNames:
+            timeOfLastUpdate = RrdUtilities.getDatabaseTimeOfUpdate( dataBaseName, fileType )
+            
+            if timeOfLastUpdate in timesOfUpdate:
+                timesOfUpdate[timeOfLastUpdate] = timesOfUpdate[timeOfLastUpdate] + 1 
+            else:
+                timesOfUpdate[timeOfLastUpdate] = 1     
+        
+        for timeOfUpdate in timesOfUpdate:
+            if timesOfUpdate[timeOfUpdate] > maximumPopularity:
+                mostPopular = timeOfUpdate
+                maximumPopularity = timesOfUpdate[timeOfUpdate]
+                 
+        return mostPopular   
+    
+    getMostPopularTimeOfLastUpdate = staticmethod( getMostPopularTimeOfLastUpdate )    
