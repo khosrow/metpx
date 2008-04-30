71cb380603c685a59629eed854a72dd97d55687b
Nicholas Lemay <nicholaslemay@sourceforge.net>
2008-04-30 15:42:14 +0000

    Updated file so that it follows recent modification to pxStats.
    Notably : 
        Now uses proper inclusion of PxLib
        Proper use of StatsPaths
        Proper paths towards images as to fit new installation paths.

--- pxStats/bin/webPages/generateImageWebPage.py
+++ pxStats/bin/webPages/generateImageWebPage.py
@@ -1,19 +1,18 @@
 #! /usr/bin/env python
 """
-MetPX Copyright (C) 2004-2006  Environment Canada
-MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
-named COPYING in the root of the source directory tree.
-
-
 ##############################################################################
 ##
 ##
 ## @Name   : generateImageWebPage.py 
 ##
+## @license : MetPX Copyright (C) 2004-2006  Environment Canada
+##            MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
+##            named COPYING in the root of the source directory tree.   
+##
 ##
 ## @author:  Nicholas Lemay
 ##
-## @since: 22-11-2006, last update on :
+## @since: 22-11-2006, last updated on : 2008-04-30
 ##
 ##
 ## Description : Generates a web pages that gives access to user 
@@ -23,32 +22,38 @@ named COPYING in the root of the source directory tree.
 ##
 ##############################################################################
 """
-
-
-"""
-    Small function that adds pxlib to the environment path.  
-"""
-import os, time, sys
-sys.path.insert(1, sys.path[0] + '/../../')
-sys.path.insert(2, sys.path[0] + '/../../..')
-
-
-try:
-    pxlib = os.path.normpath( os.environ['PXROOT'] ) + '/lib/'
-except KeyError:
-    pxlib = '/apps/px/lib/'
-    
-    
-import cgi, os, time, sys
+import os, sys
+import cgi
 import cgitb; cgitb.enable()
+
+"""
+    Small function that adds pxStats to the sys path.  
+"""
+sys.path.insert(1, sys.path[0] + '/../../..')
+
 from pxStats.lib.StatsPaths import StatsPaths
 
+"""
+    Small method required to add pxLib to syspath.
+"""
+PATHS = StatsPaths()
+PATHS.setBasicPaths()
+sys.path.append( PATHS.PXLIB ) 
+    
 
-def returnReplyToQuerier(error =""):
+
+def returnReplyToQuerier( error ="" ):
     """
         @summary : Prints an empty reply so that the receiving web page will
                    not modify it's display.
+                   
+        @param  error : Error to return to querier.            
         
+        @return : None
+   
+        @note: Method does not actually "return" anything. 
+               It just prints out it's reply that is to be 
+               intercepted by the  querier.
     """
     
     if error == "":
@@ -79,7 +84,10 @@ def generateWebPage( images ):
     smallImageWidth  = 900
     smallImageHeight = 320
     
-    file = StatsPaths.STATSWEBPAGESHTML + "combinedImageWebPage.html"
+    statsPaths = StatsPaths()
+    statsPaths.setPaths()
+        
+    file = statsPaths.STATSWEBPAGESHTML + "combinedImageWebPage.html"
     fileHandle = open( file, "w")           
     
     
@@ -148,9 +156,12 @@ def generateWebPage( images ):
     
     
 
+    relativePathTowardsPxStats = "../../../pxStats/"
     
     for i in range(len(images) ):
-    
+        
+        pathTowardsImage = str(images[i]).split( "pxStats" )[-1:][0]
+        
         fileHandle.write(""" 
             <a href="#" class="photosLink"  name="photo%s" onclick="javascript:transport('%s')" id="photo%s" border=0>
             </a>
@@ -158,7 +169,7 @@ def generateWebPage( images ):
                 document.getElementById('photo%s').style.background="url(" + "%s" + ") no-repeat";
             </script>
             
-        """%( i, images[i], i, i, images[i] ) )
+        """%( i, relativePathTowardsPxStats + pathTowardsImage, i, i,  relativePathTowardsPxStats + pathTowardsImage ) )
     
     
     
@@ -218,12 +229,11 @@ def main():
                    Replies to the querier after generating web page so that querier
                    is informed the page was generated.     
     """
-   
+
     images = getImagesFromForm()
     #print images
     generateWebPage( images )
     returnReplyToQuerier()
-
 
         
         
