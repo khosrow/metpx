b66c5a118a2a8257b8717bc88028d77a1543c6cf
Michel Grenier <michel_grenier@sourceforge.net>
2010-06-23 19:51:09 +0000

    MG creating that branch

---
+++ sundew/lib/dshLib.py
@@ -0,0 +1,63 @@
+"""
+MetPX Copyright (C) 2004-2007  Environment Canada
+MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
+named COPYING in the root of the source directory tree.
+"""
+
+"""
+#############################################################################################
+# Name: dshLib.py
+#
+# Author: Daniel Lemay
+#
+# Date: 2007-10-11
+#
+# Description: Useful dsh related functions
+#
+#############################################################################################
+"""
+import sys, commands
+import fileLib
+
+DSH_ROOT = "/etc/dsh/"
+DSH_GROUP = DSH_ROOT + "group/"
+
+def getMachines(group):
+    if group == 'pxatx':
+        try:
+            command = 'ssh pds@pxatx hostname'
+            hostname = commands.getoutput(command)
+        except:
+            hostname = None
+
+        if hostname: machines = [hostname]
+        else: machines = []
+
+    else:
+        try:
+            machines = [machine.strip() for machine in fileLib.getLines(DSH_GROUP + group)]
+        except:
+            machines = []
+    return machines
+    """
+    #return machines or ['userver1-new', 'userver2-new']
+    machines = []
+    if group.lower() == 'pxatx':
+        machines = ['pxatx']
+    elif group.lower() in ['px', 'userver', 'userv']:
+        machines = ['userver1-new', 'userver2-new']
+    """
+
+if __name__ == '__main__':
+    print getMachines("px")
+    print getMachines("pds")
+    print getMachines("pxatx")
+
+    print getMachines("PX")
+    print getMachines("PDS")
+    print getMachines("PXATX")
+
+    print getMachines("toto")
+    print getMachines("userv")
+    print getMachines("userver")
+
