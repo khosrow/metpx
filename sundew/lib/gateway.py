c4e4807782881b048eaec686795bebbe5ec80445
Michel Grenier <michel_grenier@sourceforge.net>
2012-03-05 16:05:10 +0000

    MG/JH  release for CMO packaging

---
+++ sundew/lib/gateway.py
@@ -0,0 +1,212 @@
+# -*- coding: iso-8859-1 -*-
+# MetPX Copyright (C) 2004-2006  Environment Canada
+# MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
+# named COPYING in the root of the source directory tree.
+#
+#   Auteur:      
+#
+#      2004/10 -- Louis-Philippe Thériault
+#
+
+"""abstract class for bulletin transfers """
+
+import imp, time, sys
+from MultiKeysStringSorter import MultiKeysStringSorter
+from DiskReader import DiskReader
+from Source import Source
+import PXPaths
+PXPaths.normalPaths()
+
+__version__ = '2.0'
+
+class gatewayException(Exception):
+    """what is the point?  exception class for gateways"""
+    pass
+
+class gateway:
+    """container for the common attributes of a gateway.
+       
+       Receivers and senders are derived from thie class.
+
+       self.config module will contain the obvious.
+
+       Terminology:
+
+          - from a reader we can call a data reader
+            (ex: disque, socket, etc...)
+          - we can provide data to a write which can be written
+            (ex: disque, socket, etc...)
+
+       Instanciation:
+
+       Gateway is instantiated with a config file.
+
+            path            String
+                            - path to config file.
+
+            flow            - Source or Client (permits access to all options)
+
+            logger          logging object
+
+                            - Must be callable to write messages.   
+                              this is normally the program's log file.
+
+    """
+    def __init__(self, path, flow, logger):
+        self.pathToConfigFile = path
+        self.logger = logger
+        self.flow = flow 
+        self.igniter = None
+        self.reader = None
+
+        # statistics.
+        self.totBytes = 0
+        self.initialTime = time.time()
+        self.finalTime = None
+
+
+
+    def resetReader(self):
+        self.reader = DiskReader(PXPaths.TXQ + self.flow.name,
+                                 self.flow.batch,            # Number of files we read each time
+                                 self.flow.validation,       # name validation
+                                 self.flow.patternMatching,  # pattern matching
+                                 self.flow.mtime,            # we don't check modification time
+                                 True,                       # priority tree
+                                 self.logger,
+                                 eval(self.flow.sorter),
+                                 self.flow)
+
+    def setIgniter(self, igniter):
+        self.igniter = igniter
+
+    def loadConfig(path):
+        """loadConfig(path)
+
+           Load the configuration file.
+        """
+        try:
+            fic_cfg = open(path,'r')
+            config = imp.load_source('config','/dev/null',fic_cfg)
+            fic_cfg.close()
+
+            return config
+        except IOError:
+            print "*** Error: configuration file missing !"
+            sys.exit(-1)
+
+    loadConfig = staticmethod(loadConfig)
+
+    def establishConnection(self):
+        """establishConnection()
+
+           Establish a connection with a reader and writer (check
+           that resources are available too.) is also 
+           called if the connection is dropped.
+
+        """
+        raise gatewayException('Not Implemented abstract class method establishConnection')
+
+    def read(self):
+        """read() -> data
+
+           data : Liste of objects
+
+           This method returns a list of objects to be ingested by a writer.
+           Raise an exception if there is an error.
+
+        """
+        raise gatewayException('Méthode non implantée (méthode abstraite read)')
+
+    def write(self,data):
+        """write(data)
+
+           data : Liste d'objets
+
+           write the data given (as returned by a read call.) while doing
+           the appropriate processing.
+
+        """
+        raise gatewayException('Méthode non implantée (méthode abstraite write)')
+
+    def run(self):
+        """run()
+
+           infinite loop to transfer data. An exception is raised
+           if either the reader or writer is not available.
+        """
+        while True:
+
+            try:
+                data = self.read()
+            except gatewayException, e:
+                if e.args[0] == "Le lecteur ne peut être accédé":
+                # Lecture impossible, il ne devrait plus y avoir
+                # de données en attente
+                    if len(data) > 0:
+                        self.write(data)
+                        data = []
+
+                    self.establishConnection()
+                    
+                    """
+                    # Intent of these lines were to reread information when a reconnexion occurs.
+                    # Not useful because each time a change is done, a reload or restart (that
+                    # do the reread job) must occurs. To be erased in 1 month.
+
+                    # We need (am and wmo receivers) to reread the config file, the ROUTING_TABLE (and px clients), 
+                    # and maybe the STATION_TABLE (am)
+                    if isinstance(self.flow, Source):
+                        if self.flow.type in ['am', 'wmo', 'amqp']:
+                            self.flow.__init__(self.flow.name, self.flow.logger)
+                            self.renewBulletinManager()
+                    """
+                else:
+                    raise
+
+            if len(data) == 0:
+            # S'il n'y a pas de nouveau data
+                time.sleep(0.1)
+            else:
+                self.write(data)
+
+    def shutdown(self):
+        """shutdown()
+
+           close down gateway nicely.
+
+        """
+        self.logger.info("clean gateway shutdown")
+
+    def checkLooping(self, unElementDeData):
+        """checkLooping() -> bool
+
+           detect if data given has already been received.
+
+           Return True if the object is a repeat.
+        """
+        raise gatewayException("Method not implemented: gateway.gateway.checkLooping()")
+
+    def reloadConfig(self):
+        """reloadConfig()
+
+           Reload settings from configuration files.
+
+           Purpose:
+
+                handle SIGHUP
+
+        """
+        self.logger.info('reload config file... not implemented!')
+
+    def printSpeed(self):
+        elapsedTime = time.time() - self.initialTime
+        speed = self.totBytes/elapsedTime
+        self.totBytes = 0
+        self.initialTime = time.time()
+        return "Speed = %i" % int(speed)
+
+    def tallyBytes(self,bytecount):
+        self.totBytes += bytecount
+
+    
---
+++ sundew/lib/socketManagerAm.py
@@ -0,0 +1,194 @@
+# -*- coding: iso-8859-1 -*-
+# MetPX Copyright (C) 2004-2006  Environment Canada
+# MetPX comes with ABSOLUTELY NO WARRANTY; For details type see the file
+# named COPYING in the root of the source directory tree.
+#
+# Author:
+#
+#    2004/10 -- Louis-Philippe Thériault
+#    2005/01 -- Pierre
+
+"""Spécialisation pour gestion de sockets "AM" """
+
+__version__ = '2.0'
+
+import struct, socket, curses, curses.ascii, string, time
+import socketManager
+
+class socketManagerAm(socketManager.socketManager):
+    __doc__ = socketManager.socketManager.__doc__ + \
+    """
+    implementation of AM protocol socket manager
+
+    * Attributes
+
+    patternAmRec            str
+                     - Pattern pour le parsing header struct.
+    sizeAmRec               int
+                     - header length (in octets)
+                           calculated by struct 
+
+    """
+
+    #def __init__(self,logger,type='slave',localPort=9999,remoteHost=None,timeout=None):
+            #socketManager.socketManager.__init__(self,logger,type,localPort,remoteHost,timeout)
+    def __init__(self,logger,type='slave',port=9999,remoteHost=None,timeout=None, flow=None):
+        socketManager.socketManager.__init__(self,logger,type,port,remoteHost,timeout, flow)
+
+        # The size of amRec is take from the (not included) file ytram.h, originally from
+        # amtcp2file (also not included in application.) to manage the fields we use
+        # python struct.
+        #self.patternAmRec = '80sLL4sii4s4s20s'
+        #self.patternAmRec = '80sLL4siiii20s'
+        self.patternAmRec = '80sII4siIII20s'
+        self.sizeAmRec = struct.calcsize(self.patternAmRec)
+
+    def unwrapBulletin(self):
+        __doc__ = socketManager.socketManager.unwrapBulletin.__doc__ + \
+        """
+        """
+        status = self.checkNextMsgStatus()
+
+        if status == 'OK':
+            (header,src_inet,dst_inet,threads,start,length,firsttime,timestamp,future) = \
+                     struct.unpack(self.patternAmRec,self.inBuffer[0:self.sizeAmRec])
+
+            length = socket.ntohl(length)
+
+            bulletin = self.inBuffer[self.sizeAmRec:self.sizeAmRec + length]
+
+            return (bulletin,self.sizeAmRec + length)
+        else:
+            return '',0
+
+    def wrapBulletin(self,bulletin,thread_number):
+        __doc__ = socketManager.socketManager.wrapBulletin.__doc__ + \
+        """
+           wrapBulletin
+
+           input Parameters:
+               -bulletin:   a bulletinAm object
+
+           return value:
+           -a bulletin as a ready to send string.
+
+           Purpose:
+           Add appropriate AM header to input bulletin.
+
+        """
+        #initialisation des header fields
+        #char header[80]
+        tmp = bulletin.getBulletin()
+        size = struct.calcsize('80s')
+
+        nulList = [chr(curses.ascii.NUL) for x in range(size)]
+        header = list(tmp[0:size])
+
+        paddedHeader = header + nulList[len(header):]
+
+        header = string.join(paddedHeader,'')
+        header = header.replace(chr(curses.ascii.LF), chr(curses.ascii.NUL), 1)
+
+        #unsigned long src_inet, dst_inet
+        src_inet = 0
+        dst_inet = 0
+
+        #unsigned char threads[4]
+        #threads='0'+chr(255)+'0'+'0'
+        threads= chr(0) + chr(thread_number) + chr(0) + chr(0)
+
+        #unsigned int start, length
+        start = 0
+        length = socket.htonl( len(bulletin.getBulletin()) )
+
+        #time_t firsttime, timestamp
+        #firsttime = chr(curses.ascii.NUL)
+        #timestamp = chr(curses.ascii.NUL)
+        firsttime = socket.htonl(int(time.time()))
+        timestamp = socket.htonl(int(time.time()))
+
+        #char future[20]
+        future = chr(curses.ascii.NUL)
+
+        #construction de l'entete
+        bulletinHeader = struct.pack(self.patternAmRec,header,src_inet,dst_inet,threads,start \
+                                ,length,firsttime,timestamp,future)
+
+        #put header and content together.
+        wrappedBulletin = bulletinHeader + bulletin.getBulletin()
+
+        return wrappedBulletin
+
+    def checkNextMsgStatus(self):
+        __doc__ = socketManager.socketManager.checkNextMsgStatus.__doc__ + \
+        """
+           Does not detect data corruption, AM protocol weakness?
+
+        """
+        if len(self.inBuffer) >= self.sizeAmRec:
+            (header,src_inet,dst_inet,threads,start,length,firsttime,timestamp,future) = \
+                    struct.unpack(self.patternAmRec,self.inBuffer[0:self.sizeAmRec])
+        else:
+            return 'INCOMPLETE'
+
+        length = socket.ntohl(length)
+
+        if len(self.inBuffer) >= self.sizeAmRec + length:
+            return 'OK'
+        else:
+            return 'INCOMPLETE'
+
+    def sendBulletin(self,bulletin,thread_number=255):
+        #__doc__ = socketManager.socketManager.sendBulletin.__doc__ + \
+        """
+        input parameter:
+           -bulletin:
+
+        return value:
+        -on success: 0
+        -on failure: 1
+
+        FIXME: documentation wrong about return value, also sends # bytes sent.
+
+        Purpose:
+        Send a bulletin to a socket.  Verify connection status on each send.
+        
+        """
+        try:
+            #prepare bulletin for send
+            data = self.wrapBulletin(bulletin,thread_number)
+
+            #print repr(data)
+            #print('=====================================================================')
+
+            # try to send , verify connection via try/except.
+            try:
+                bytesSent = self.socket.send(data)
+
+                #check if it went OK.
+                if bytesSent != len(data):
+                    self.connected=False
+                    return (0, bytesSent)
+                else:
+                    return (1, bytesSent)
+
+            except socket.error, e:
+                #possible errors: 104, 107, 110 or 32
+                self.logger.error("senderAm.write(): connection broken: %s",str(e.args))
+                self.connected = False
+                self.logger.info("senderAm.write(): attempt to reconnect")
+                self.socket.close()
+                self._socketManager__establishConnection()
+
+        except Exception, e:
+            self.logger.error("socketManagerAm.sendBulletin(): send error: %s",str(e.args))
+            raise
+
+    def writetofile(self,filename,data):
+           import sys, os
+           import random 
+           r = random.random()
+           str_r = '%f' % r
+           unFichier = os.open( filename + str_r, os.O_CREAT | os.O_WRONLY )
+           os.write( unFichier , data )
+           os.close( unFichier )
